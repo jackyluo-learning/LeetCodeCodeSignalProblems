e5f5493058419b32499925adc6ec2b926971e8fe
luoxuqi <1576123082@qq.com>
2020-02-15 22:22:53 +0800

    updated

--- src/ValidParentheses.java
+++ src/ValidParentheses.java
@@ -6,6 +6,14 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 
 public class ValidParentheses {
+    /*
+    Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+    An input string is valid if:
+        1. Open brackets must be closed by the same type of brackets.
+        2. Open brackets must be closed in the correct order.
+    Note that an empty string is also considered valid.
+     */
+
     @Rule
     public MyJUnitStopWatch stopwatch = new MyJUnitStopWatch();
 
